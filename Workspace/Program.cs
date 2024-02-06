bdb087748a64ccc8bc68b13919faf0783d9891f0
SamirAlizada <samir.elizade5555@gmail.com>
2024-02-06 12:56:28 +0400

    Add project files.

---
+++ Workspace/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workspace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Samir";
+            person1.LastName = "Alizada";
+            person1.DateOfBirthYear = 2003;
+            person1.NationalIdentity = 123456789;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+
+     
+    }
+}
